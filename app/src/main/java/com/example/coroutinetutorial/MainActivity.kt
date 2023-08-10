0975984b40c937c0624dea3be31832524d075696
Ibrahimnadra <nadra.ibrahim@knoldus.com>
2023-08-10 17:46:52 +0530

    Step 2 : Suspending Coroutines

--- app/src/main/java/com/example/coroutinetutorial/MainActivity.kt
+++ app/src/main/java/com/example/coroutinetutorial/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.coroutinetutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch{
+            val answer1 = networkCall1()
+            val answer2 = networkCall2()
+            Log.d(TAG, "Answer1 is $answer1")
+            Log.d(TAG, "Answer2 is $answer2")
+        }
+    }
+
+   suspend fun networkCall1(): String {
+       delay(3000L)
+       return "This is network call 1"
+   }
+
+    suspend fun networkCall2(): String {
+        delay(3000L)
+        return "This is network call 2"
     }
 }
