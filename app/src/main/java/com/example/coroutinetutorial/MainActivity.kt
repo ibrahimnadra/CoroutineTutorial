3212a16b03c5551be40bb72bb45b9b64f9021bf3
Ibrahimnadra <nadra.ibrahim@knoldus.com>
2023-08-10 17:54:48 +0530

    Step 3 : Understanding Context and Dispatchers

--- app/src/main/java/com/example/coroutinetutorial/MainActivity.kt
+++ app/src/main/java/com/example/coroutinetutorial/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.coroutinetutorial
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView1 = findViewById<TextView>(R.id.textView1)
+
+        GlobalScope.launch{
+            Log.d(TAG, "Coroutine is started in thread : ${Thread.currentThread().name}")
+            val answer = networkCall()
+            withContext(Dispatchers.Main){
+                Log.d(TAG, "Settting text in thread : ${Thread.currentThread().name}")
+                textView1.text = answer
+            }
+        }
     }
+
+    suspend fun networkCall(): String {
+       delay(3000L)
+       return "This is network call 1"
+   }
 }
