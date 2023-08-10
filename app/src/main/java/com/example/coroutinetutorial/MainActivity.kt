4d755add5af5d2552658af8e8dad574579039643
Ibrahimnadra <nadra.ibrahim@knoldus.com>
2023-08-10 18:12:56 +0530

    Step 4 : Using `runBlocking`

--- app/src/main/java/com/example/coroutinetutorial/MainActivity.kt
+++ app/src/main/java/com/example/coroutinetutorial/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.coroutinetutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "Before runBlocking has started.")
+
+        runBlocking {
+            launch(Dispatchers.IO) {
+                delay(3000L)
+                Log.d(TAG, "Finished first IO coroutine")
+            }
+            launch(Dispatchers.IO) {
+                delay(3000L)
+                Log.d(TAG, "Finished second IO coroutine")
+            }
+            Log.d(TAG, "Start of runBlocking")
+            delay(5000L)
+            Log.d(TAG, "End of runBlocking")
+        }
+
+        Log.d(TAG, "After runBlocking is finished.")
     }
 }
