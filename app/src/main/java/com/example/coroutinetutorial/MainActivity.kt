a7030c70881a4e214457c088ee8b2f40d4858e5c
Ibrahimnadra <nadra.ibrahim@knoldus.com>
2023-08-10 17:42:26 +0530

    Getting Started with Coroutines

--- app/src/main/java/com/example/coroutinetutorial/MainActivity.kt
+++ app/src/main/java/com/example/coroutinetutorial/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.coroutinetutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch{
+            Log.d(TAG, "Greetings from coroutine (thread : ${Thread.currentThread().name}")
+        }
+        Log.d(TAG, "Greetings from MainActivity (thread : ${Thread.currentThread().name}")
     }
 }
