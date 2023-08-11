a9fe08dacc12bca884c8538d99279619792f4b08
Ibrahimnadra <nadra.ibrahim@knoldus.com>
2023-08-11 18:10:46 +0530

    Step 7 :Coroutine Scopes:

--- app/src/main/java/com/example/coroutinetutorial/MainActivity.kt
+++ app/src/main/java/com/example/coroutinetutorial/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.coroutinetutorial
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener(){
+//            ViewModelScope.launch {
+            lifecycleScope.launch {
+                while(true){
+                    delay(1000L)
+                    Log.d(TAG, "Still Running")
+                }
+            }
+            GlobalScope.launch {
+                delay(5000L)
+                Intent(this@MainActivity, SecondActivity::class.java).also {
+                    startActivity(it)
+                    finish()
+                }
+            }
+        }
     }
 }
